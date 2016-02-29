c155b9cc1980f99a4a8eb3a65c7f816162ca7241
Sergey <sdgaykov@gmail.com>
2016-02-29 15:30:50 +0300

    add radios to change mazes, style button

--- js/index.js
+++ js/index.js
@@ -1,23 +1,50 @@
 (function (root) {
-    var map = root.maze.MAZE_Y;
-    var path = root.maze.solution(map, 1, 0);
+    var map = root.maze.MAZE_21;
 
     document.querySelector('.outer').appendChild(
         root.maze.render(map, null)
     );
 
     document.getElementById('run').onclick = function() {
-        visualize(0);
+        visualize(root.maze.solution(map, 1, 0), 0);
     };
 
-    function visualize(index) {
+    for (var i = 0, radios = document.radios.radio; i < radios.length; ++i) {
+        radios[i].onclick = function(e) {
+            changeMaze(e.srcElement.defaultValue);
+        }
+    }
+
+    function visualize(path, index) {
         var cellToHighlight = path[index];
         var cell = document.getElementById(cellToHighlight[0] + '-' + cellToHighlight[1]);
         cell.className += ' maze__cell_path';
 
         if(index === path.length - 1) return;
 
-        setTimeout(function() {visualize(index + 1)}, 50);
+        setTimeout(function() {visualize(path, index + 1)}, 50);
+    }
+
+    function changeMaze(newMaze) {
+        switch (newMaze) {
+            case '21':
+                map = root.maze.MAZE_21;
+                break;
+            case '31':
+                map = root.maze.MAZE_31;
+                break;
+            case '51':
+                map = root.maze.MAZE_51;
+                break;
+            default:
+                map = root.maze.MAZE_Y;
+        }
+
+        var outer = document.querySelector('.outer');
+        outer.innerHTML = '';
+        outer.appendChild(
+            root.maze.render(map, null)
+        );
     }
 
 })(this);
