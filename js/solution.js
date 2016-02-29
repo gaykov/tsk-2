25ac6d1cdf67c1bd2a6be02afdf3fb2467db7f8a
Sergey <sdgaykov@gmail.com>
2016-02-29 14:28:35 +0300

    Init and implement recursive algoritgh

--- js/solution.js
+++ js/solution.js
@@ -4,6 +4,63 @@
     var PATH = root.maze.PATH;
     var CURRENT = root.maze.CURRENT;
 
+    function RecursiveSolver(maze) {
+        this.maze = maze;
+        this.wasHere = [];
+        this.correctPath =[];
+
+        for (var i = 0; i <  maze.length; ++i) {
+            var t = [];
+            for (var j = 0; j < maze[i].length; ++j) {
+                t.push(false);
+            }
+            this.wasHere.push(t);
+        }
+
+        this.solve = function(x, y) {
+            if (y == this.maze.length - 1 && this.maze[y][x] == 0) {
+                this.correctPath.push([x, y]);
+                return true;
+            }
+
+            if (this.maze[y][x] == WALL || this.wasHere[y][x]) {
+                return false;
+            }
+
+            this.wasHere[y][x] = true;
+
+            if (y != this.maze.length - 1) {
+                if (this.solve(x, y + 1)) {
+                    this.correctPath.push([x, y]);
+                    return true;
+                }
+            }
+
+            if (x != this.maze[0].length - 1) {
+                if (this.solve(x + 1, y)) {
+                    this.correctPath.push([x, y]);
+                    return true;
+                }
+            }
+
+            if (y != 0) {
+                if (this.solve(x, y - 1)) {
+                    this.correctPath.push([x, y]);
+                    return true;
+                }
+            }
+
+            if (x != 0) {
+                if (this.solve(x - 1, y)) {
+                    this.correctPath.push([x, y]);
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+
     /**
      * Функция находит путь к выходу и возвращает найденный маршрут
      *
@@ -14,10 +71,9 @@
      */
     function solution(maze, x, y) {
         // todo: построить правильный маршрут к выходу
-        return [
-            [1, 0],
-            [1, 1]
-        ];
+        var t = new RecursiveSolver(maze);
+        t.solve(x, y);
+        return t.correctPath;
     }
 
     root.maze.solution = solution;
